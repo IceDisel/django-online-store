e74bc1fb7b734938f91998e576970d78f906ab12
Vladimir Titov <tvv-vg@yandex.ru>
2024-04-22 21:12:34 +0300

    add forms

---
+++ catalog/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    forbid_words = ("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар",)
+
+    def clean_name(self):
+        cleaned_name = self.cleaned_data['name']
+        for name in self.forbid_words:
+            if name in cleaned_name:
+                raise forms.ValidationError("Недопустимое имя товара")
+        return cleaned_name
+
+    def clean_description(self):
+        cleaned_description = self.cleaned_data['description']
+        for description in self.forbid_words:
+            if description in cleaned_description:
+                raise forms.ValidationError(f"Недопустимое описание")
+        return cleaned_description
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
