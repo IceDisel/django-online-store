e9e6dab0042038be89fdeb0c81814e126af005c4
Vladimir Titov <tvv-vg@yandex.ru>
2024-04-22 21:09:57 +0300

    add model version

--- catalog/models.py
+++ catalog/models.py
@@ -33,3 +33,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    number_ver = models.CharField(max_length=50, **NULLABLE, verbose_name='Номер версии')
+    name_ver = models.CharField(max_length=100, verbose_name='Название версии')
+    activ_ver = models.BooleanField(default=False, verbose_name='Активная версия')
+
+    def __str__(self):
+        return self.name_ver
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
