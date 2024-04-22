44b6dd558176090b088962ab4a035165e8f5031a
Vladimir Titov <tvv-vg@yandex.ru>
2024-04-22 21:13:19 +0300

    update views

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from catalog.models import Product
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Version
 
 
 class ProductListView(ListView):
@@ -13,6 +15,12 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'BitStore - Каталог'
+
+        context['object_list'] = Product.objects.all()
+
+        for product in context['object_list']:
+            product.active_version = Version.objects.filter(product=product, activ_ver=True).last()
+
         return context
 
 
@@ -24,6 +32,18 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'BitStore - Товар'
         return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:index')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:index')
 
 
 class ContactsView(View):
@@ -48,36 +68,9 @@ class ContactsView(View):
         }
         return render(request, self.template_name, context)
 
-# def index(request):
-#     products_list = Product.objects.all()
-#
-#     context = {
-#         'object_list': products_list,
-#         'title': 'BitStore - Каталог',
-#     }
-#     return render(request, 'catalog/index.html', context)
 
+class VersionCreateView(CreateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy('catalog:index')
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(name, phone, message)
-#
-#     context = {
-#         'title': 'BitStore - Контакты',
-#         'title_1': 'Контакты',
-#     }
-#
-#     return render(request, 'catalog/contacts.html', context)
-
-
-# def product(request, pk):
-#     products_list = Product.objects.get(pk=pk)
-#
-#     context = {
-#         'object_list': products_list,
-#         'title': 'BitStore - Товар',
-#     }
-#     return render(request, 'catalog/product.html', context)
