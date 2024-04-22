ae1efe58e5d8255a9ca0a7c3e660af3b1de83538
Vladimir Titov <tvv-vg@yandex.ru>
2024-04-22 21:12:01 +0300

    add urls ProductCreate, ProductUpdate, VersionCreate

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactsView
+from catalog.views import ProductListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, VersionCreateView
 
 app_name = CatalogConfig.name
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
-
+    path('product_form/', ProductCreateView.as_view(), name='product_form'),
+    path('product_form/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
+    path('version_form', VersionCreateView.as_view(), name='version_form')
 ]
